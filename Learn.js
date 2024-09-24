9cf67a1c2784ac64e70081764dcbd1bab7f81a93
Sanvi <94829994+SanviGitHub@users.noreply.github.com>
2024-09-24 01:22:19 -0300

    Add files via upload

---
+++ Learn.js
@@ -0,0 +1,5 @@
+// Ejemplo de código JavaScript para agregar funcionalidad
+document.addEventListener("DOMContentLoaded", function () {
+    // Aquí puedes agregar código para interactividad
+    console.log("Página Learn.js cargada correctamente.");
+});
